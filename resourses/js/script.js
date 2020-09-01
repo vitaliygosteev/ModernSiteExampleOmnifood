2f0679370f2fc384763bdd57bb3286e7c5e89b03
Vitaliy Gosteev <vitaliygosteev@gmail.com>
2020-09-02 01:13:19 +0500

    Added animations

--- resourses/js/script.js
+++ resourses/js/script.js
@@ -40,4 +40,38 @@ $(document).ready(function()  {
         });
     });
 
+    /* Scroll animations */
+
+    // Features
+    $('.js--wp-1').waypoint(function(direction) {
+        $('.js--wp-1').addClass('animate__animated animate__fadeIn');
+    },
+    {
+        offset: '50%'
+    });
+
+    // iPhone app
+    $('.js--wp-2').waypoint(function(direction) {
+        $('.js--wp-2').addClass('animate__animated animate__fadeInUp');
+    },
+    {
+        offset: '50%'
+    });
+
+    // Cities
+    $('.js--wp-3').waypoint(function(direction) {
+        $('.js--wp-3').addClass('animate__animated animate__fadeIn');
+    },
+    {
+        offset: '50%'
+    });
+
+    // Plan box
+    $('.js--wp-4').waypoint(function(direction) {
+        $('.js--wp-4').addClass('animate__animated animate__pulse');
+    },
+    {
+        offset: '50%'
+    });
+
 });
