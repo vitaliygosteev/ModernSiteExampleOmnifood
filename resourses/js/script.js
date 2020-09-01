7c5a195dd5d5e2e8d46a7c578668a1768ab62dec
Vitaliy Gosteev <vitaliygosteev@gmail.com>
2020-09-02 00:17:06 +0500

    Added navigation scroll

--- resourses/js/script.js
+++ resourses/js/script.js
@@ -1,5 +1,7 @@
 $(document).ready(function()  {
 
+    /* Sticky navigation */
+
     $('.js--section-features').waypoint(function(direction)  {
         if (direction == "down") {
             $('nav').addClass('sticky');
@@ -8,6 +10,34 @@ $(document).ready(function()  {
         }
     }, {
         offset: '60px;'
+    });
+
+    /* Scroll on buttons */
+
+    /* I'm hungry */
+    $('.js--scroll-to-plans').click(function() {
+        $('html, body').animate({scrollTop: $('.js--section-plans').offset().top}, 1000);
+    });
+
+    /* Show me */
+    $('.js--scroll-to-start').click(function() {
+        $('html, body').animate({scrollTop: $('.js--section-features').offset().top}, 1000);
+    });
+
+    /* Navigation button's scroll */
+
+    $(document).ready(function(){
+        $("a").on('click', function(event) {
+            if (this.hash !== "") {
+                event.preventDefault();
+                var hash = this.hash;
+                    $('html, body').animate({
+                    scrollTop: $(hash).offset().top
+                    }, 1000, function(){
+                    window.location.hash = hash;
+                    });
+            }
+        });
     });
 
 });
